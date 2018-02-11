7ef0a370305059c597ba975858a34ba54ab5a876
Luis Garcia <garcia@yahoo.com>
2018-02-11 09:31:03 -0700

    Login Page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
   validates :name, presence: true 
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: { message: "This email already exists in our database!" }
   validates :password_digest, presence: true
   has_many :categories
   has_many :ideas 
---
+++ spec/features/authentication_user_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe "As an unathenticted use" do
+  describe "when I visit the landing page I see a create a new user link" do
+    scenario "when I click on the link I can create an account" do
+      visit '/'
+
+      click_on 'Sign Up To Be A User'
+
+      expect(current_path).to eq(new_user_path)
+      expect(page).to have_content("Create a user here") 
+
+
+      fill_in "user[name]", with: "Ed"
+      fill_in "user[email", with: "muf@fin.com"
+      fill_in "user[password]", with: "muffin"
+
+      click_on "Create User"
+
+      expect(current_path).to eq(user_path(User.last.id))   
+      expect(page).to have_content("Welcome Ed") 
+    end
+  end
+end
